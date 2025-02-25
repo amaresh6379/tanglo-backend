6bd7b91590281b4d5b635f3a75a0d056ff458115
Amareshraja <amareshraja@centizen.com>
2025-02-25 22:38:35 +0530

    feat :  initial commit

---
+++ msg.js
@@ -0,0 +1,9 @@
+const MSG = {};
+
+MSG.CREATE_USER_SUCCESS = {
+    message: 'user creation success'
+}
+MSG.CREATE_USER_FAILED = {
+    message: 'user creation failed'
+}
+module.exports.MSG = MSG;
