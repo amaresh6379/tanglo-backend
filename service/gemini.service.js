2bf68dc31a834bb28bfa1aa066d358fe77390b90
Amareshraja <amareshraja@centizen.com>
2025-02-25 23:11:11 +0530

    code

--- service/gemini.service.js
+++ service/gemini.service.js
@@ -1,42 +1,41 @@
-const apiKey  = "AIzaSyB8VjCp5yf8NHxqzxnyHpZgYoJ39uUWvY0";
-const apiUrl  = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyB8VjCp5yf8NHxqzxnyHpZgYoJ39uUWvY0";
-const axios =  require('axios');
+
+const axios = require('axios');
 
 const getConveration = async () => {
-    let contents = [
-        {
-            "parts":[
-                {
-                    "text": "I am creating a one-on-one Tanglish chat between a boss and an employee. The AI should act as one of the participants, asking only one question at a time based on the user's input. The response should feel natural and conversational, in fluent Tanglish, without including direct Tamil script or multiple unrelated questions. The AI should not make up an entire conversation; it must only ask one question or respond based on the most recent input from the user to create a real-time back-and-forth chat experience."
-                }
-            ]
-
-        }
-    ];
-    // contents[0].parts.push(data);
-    const  [converstionErr,converstionData]  = await to(axios.post(apiUrl,{contents},
+  let contents = [
     {
-            headers: {'Content-Type': 'application/json'},
-            params: {key: apiKey} 
-    }));
-    if(converstionErr) {
-        console.log(converstionErr);
-        return converstionErr;
+      "parts": [
+        {
+          "text": "I am creating a one-on-one Tanglish chat between a boss and an employee. The AI should act as one of the participants, asking only one question at a time based on the user's input. The response should feel natural and conversational, in fluent Tanglish, without including direct Tamil script or multiple unrelated questions. The AI should not make up an entire conversation; it must only ask one question or respond based on the most recent input from the user to create a real-time back-and-forth chat experience."
+        }
+      ]
+
     }
-    return converstionData;
+  ];
+  // contents[0].parts.push(data);
+  const [converstionErr, converstionData] = await to(axios.post(apiUrl, { contents },
+    {
+      headers: { 'Content-Type': 'application/json' },
+      params: { key: apiKey }
+    }));
+  if (converstionErr) {
+    console.log(converstionErr);
+    return converstionErr;
+  }
+  return converstionData;
 
 }
 
 
 
-const conversationWithGemini = async (req,res) => {
-    const [err,data] = await to(getConveration());
-    if(err){
-       return TE(err.message);
-    }
-    if(data){
-       return (data.data);
-    }
+const conversationWithGemini = async (req, res) => {
+  const [err, data] = await to(getConveration());
+  if (err) {
+    return TE(err.message);
+  }
+  if (data) {
+    return (data.data);
+  }
 }
 
 module.exports.conversationWithGemini = conversationWithGemini;
