38aff30ea20f0ca0e620c991321d0885429cc828
Amareshraja <amareshraja@centizen.com>
2025-02-27 21:30:49 +0530

    fix : code for tanglish

--- config.js
+++ config.js
@@ -6,17 +6,17 @@ CONFIG = {};
 // app configuration
 CONFIG.ENVIRONMENT = process.env.APP || 'local';
 CONFIG.port = process.env.PORT || 5000;
-CONFIG.jwt_encryption = process.env.jwt_encryption ;
+CONFIG.jwt_encryption = process.env.jwt_encryption || 'zenAdminEncryption';
 CONFIG.jwt_expiration = process.env.jwt_expiration || 100000
 
 
 // db configuration
-CONFIG.db_name = process.env.DB_NAME;
-CONFIG.db_user = process.env.DB_USER;
-CONFIG.db_password = process.env.DB_PASSWORD;
-CONFIG.db_host = process.env.DB_HOST;
-CONFIG.db_port = process.env.DB_PORT;
-CONFIG.db_dialect = process.env.DB_DIALECT;
+CONFIG.db_name = process.env.DB_NAME || 'defaultdb';
+CONFIG.db_user = process.env.DB_USER || 'avnadmin';
+CONFIG.db_password = process.env.DB_PASSWORD || 'AVNS_6pcbqDRus8yFs09lOl-';
+CONFIG.db_host = process.env.DB_HOST || 'pg-1d184d16-amareshraja43-139f.g.aivencloud.com';
+CONFIG.db_port = process.env.DB_PORT || '5001';
+CONFIG.db_dialect = process.env.DB_DIALECT || 'postgres';
 CONFIG.min_pool_connection = 1;
 CONFIG.max_pool_connection = 10;
 
@@ -24,12 +24,6 @@ CONFIG.max_pool_connection = 10;
 CONFIG.client_id = process.env.GOOGLE_CLIENT_ID;
 
 
-// Bucket Name
-CONFIG.S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
-
-// Mail Configuration
-CONFIG.mail_user = process.env.MAIL_USER;
-CONFIG.mail_password = process.env.MAIL_PASSWORD;
 
 
 
