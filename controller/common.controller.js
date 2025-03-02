85af1572cc34e78076f8737365373c70b56726df
Amareshraja <amareshraja@centizen.com>
2025-03-02 23:53:31 +0530

    fix :  code changes

--- controller/common.controller.js
+++ controller/common.controller.js
@@ -1,43 +1,64 @@
 const router = require('express').Router();
 const CommonService = require('../service/common.service');
 const MSG = require('../msg').MSG;
-const passport = require('passport');
+const cron = require("node-cron");
+const axios = require('axios');
+
 /**
  * Function used to get particular section content.
  */
-const getSectionHint = async function(req,res){
-    const [err,data]  = await to(CommonService.getSectionHint(req));
-    if (err) {
-        return ReE(res, Object.assign(MSG.CREATE_USER_FAILED,{ details: err.message }), 422);
-    }
-    return ReS(res, { userToken: data, code: 200, message: MSG.CREATE_USER_SUCCESS.message }, 200); 
+const getSectionHint = async function (req, res) {
+  const [err, data] = await to(CommonService.getSectionHint(req));
+  if (err) {
+    return ReE(res, Object.assign(MSG.CREATE_USER_FAILED, { details: err.message }), 422);
+  }
+  return ReS(res, { userToken: data, code: 200, message: MSG.CREATE_USER_SUCCESS.message }, 200);
 }
 
 
 /**
  * Function used to get particular section content.
  */
-const getContentMistake = async function(req,res){
-    const [err,data]  = await to(CommonService.getContentMistake(req));
-    if (err) {
-        return ReE(res, Object.assign(MSG.CREATE_USER_FAILED,{ details: err.message }), 422);
-    }
-    return ReS(res, { userToken: data, code: 200, message: MSG.CREATE_USER_SUCCESS.message }, 200); 
+const getContentMistake = async function (req, res) {
+  const [err, data] = await to(CommonService.getContentMistake(req));
+  if (err) {
+    return ReE(res, Object.assign(MSG.CREATE_USER_FAILED, { details: err.message }), 422);
+  }
+  return ReS(res, { userToken: data, code: 200, message: MSG.CREATE_USER_SUCCESS.message }, 200);
 }
 
 /**
  * Function used to check the mistake and provide the score.
  */
-const checkMistakes = async function(req,res){
-    const [err,data]  = await to(CommonService.checkMistakes(req));
-    if (err) {
-        return ReE(res, Object.assign(MSG.CREATE_USER_FAILED,{ details: err.message }), 422);
-    }
-    return ReS(res, { userToken: data, code: 200, message: MSG.CREATE_USER_SUCCESS.message }, 200); 
+const checkMistakes = async function (req, res) {
+  const [err, data] = await to(CommonService.checkMistakes(req));
+  if (err) {
+    return ReE(res, Object.assign(MSG.CREATE_USER_FAILED, { details: err.message }), 422);
+  }
+  return ReS(res, { userToken: data, code: 200, message: MSG.CREATE_USER_SUCCESS.message }, 200);
 }
 
-router.get('/hint',getSectionHint);
-router.get('/mistake',getContentMistake);
-router.post('/content/:contentId', checkMistakes)
+const healthCheck = async function (req, res) {
+  return ReS(res, { userToken: data, code: 200, message: MSG.CREATE_USER_SUCCESS.message }, 200);
+}
 
-module.exports = {router,getSectionHint};
+const callHealthcheck = async function () {
+  try {
+    const data = await axios.get('https://tanglo-backend-1.onrender.com/v1/day');
+    if (data) {
+      console.log("Cron job is successfully exceuted.");
+    }
+  }
+  catch (err) {
+    console.log("Error", err);
+  }
+}
+
+const job = cron.schedule("*/5 * * * *", callHealthcheck);
+
+router.get('/hint', getSectionHint);
+router.get('/mistake', getContentMistake);
+router.post('/content/:contentId', checkMistakes);
+router.get('health-check', healthCheck);
+
+module.exports = { router, getSectionHint };
